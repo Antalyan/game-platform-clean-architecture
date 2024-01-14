7e1e90d14c1736dbc88988c64f6ec5de1aaa9175
Michael Koudela <xkoudel2@fi.muni.cz>
2024-01-14 20:47:35 +0100

    Validate deck card assignment

---
+++ GameSystem/src/Application/GameContext/Commands/GameDeckCommands/AddCardToDeckCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using GameSystem.Application.Common.Interfaces;
+
+namespace GameSystem.Application.GameContext.Commands.GameDeckCommands;
+
+public class AddCardToDeckCommandValidator : AbstractValidator<AddCardToDeckCommand>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IUser _user;
+    
+    public AddCardToDeckCommandValidator(IApplicationDbContext context, IUser user)
+    {
+        _context = context;
+        _user = user;
+
+        RuleFor(command => command.DeckId)
+            .NotEmpty()
+            .MustAsync((deckId, cancellationToken) =>
+                _context.Games
+                    .Where(g => g.Id == deckId)
+                    .AllAsync(g => g.CreatedBy == _user.Id, cancellationToken))
+            .WithMessage("Given id must be correspond to a deck created by this user");
+    }
+}
